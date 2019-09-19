dc1a0ac0df47aceed057f4a1712d5408c4b54e2e
Rohit Narayanan <rohit.nn@gmail.com>
2019-09-19 12:55:57 -0500

    Code for rohitnarayanan/accelerate-commons#1

--- src/main/java/accelerate/commons/util/DateTimeUtils.java
+++ src/main/java/accelerate/commons/util/DateTimeUtils.java
@@ -1,5 +1,14 @@
 package accelerate.commons.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import accelerate.commons.constant.CommonConstants;
+import accelerate.commons.exception.ApplicationException;
+
 /**
  * Class providing utility methods for Date/Time operations
  * 
@@ -8,6 +17,54 @@ package accelerate.commons.util;
  * @since January 14, 2015
  */
 public final class DateTimeUtils {
+	/**
+	 * @param aDateString
+	 * @param aFormat
+	 * @param aLocale
+	 * @param aTimezone
+	 * @return
+	 */
+	public static final Date parseDate(String aDateString, String aFormat, Locale aLocale, String aTimezone) {
+		if (StringUtils.isEmpty(aDateString)) {
+			return null;
+		}
+
+		SimpleDateFormat formatter = StringUtils.isEmpty(aFormat) ? new SimpleDateFormat()
+				: (aLocale == null) ? new SimpleDateFormat(aFormat) : new SimpleDateFormat(aFormat, aLocale);
+
+		if (!StringUtils.isEmpty(aTimezone)) {
+			formatter.setTimeZone(TimeZone.getTimeZone(aTimezone));
+		}
+
+		try {
+			return formatter.parse(aDateString);
+		} catch (ParseException error) {
+			throw new ApplicationException(error);
+		}
+	}
+
+	/**
+	 * @param aDate
+	 * @param aFormat
+	 * @param aLocale
+	 * @param aTimezone
+	 * @return
+	 */
+	public static final String formatDate(Date aDate, String aFormat, Locale aLocale, String aTimezone) {
+		if (aDate == null) {
+			return CommonConstants.EMPTY_STRING;
+		}
+
+		SimpleDateFormat formatter = StringUtils.isEmpty(aFormat) ? new SimpleDateFormat()
+				: (aLocale == null) ? new SimpleDateFormat(aFormat) : new SimpleDateFormat(aFormat, aLocale);
+
+		if (!StringUtils.isEmpty(aTimezone)) {
+			formatter.setTimeZone(TimeZone.getTimeZone(aTimezone));
+		}
+
+		return formatter.format(aDate);
+	}
+
 	/**
 	 * @param aMilliseconds
 	 * @return
--- src/test/java/accelerate/commons/util/DateTimeUtilsTests.java
+++ src/test/java/accelerate/commons/util/DateTimeUtilsTests.java
@@ -1,6 +1,13 @@
 package accelerate.commons.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +20,73 @@ import org.junit.jupiter.api.Test;
  */
 @SuppressWarnings("static-method")
 class DateTimeUtilsTests {
+	/**
+	 * Test method for
+	 * {@link DateTimeUtils#parseDate(String, String, Locale, String)}.
+	 * 
+	 * @throws ParseException
+	 */
+	@Test
+	void testParseDate() throws ParseException {
+		// empty date string
+		assertNull(DateTimeUtils.parseDate(null, null, null, null));
+
+		// only date
+		Date sample = new Date();
+		String sampleString = new SimpleDateFormat().format(sample);
+		assertEquals(new SimpleDateFormat().parse(sampleString).getTime(),
+				DateTimeUtils.parseDate(sampleString, null, null, null).getTime());
+
+		// with format
+		String format = "MM/dd/yyyy HH:ss:SSS z";
+		sampleString = new SimpleDateFormat(format).format(sample);
+		assertEquals(new SimpleDateFormat(format).parse(sampleString).getTime(),
+				DateTimeUtils.parseDate(sampleString, format, null, null).getTime());
+
+		// with locale
+		Locale locale = Locale.FRANCE;
+		sampleString = new SimpleDateFormat(format, locale).format(sample);
+		assertEquals(new SimpleDateFormat(format, locale).parse(sampleString).getTime(),
+				DateTimeUtils.parseDate(sampleString, format, locale, null).getTime());
+
+		// with timezone
+		TimeZone timezone = TimeZone.getTimeZone("UTC");
+		SimpleDateFormat formatter = new SimpleDateFormat(format, locale);
+		formatter.setTimeZone(timezone);
+		sampleString = formatter.format(sample);
+		assertEquals(formatter.parse(sampleString).getTime(),
+				DateTimeUtils.parseDate(sampleString, format, locale, "UTC").getTime());
+	}
+
+	/**
+	 * Test method for
+	 * {@link DateTimeUtils#formatDate(Date, String, Locale, String)}.
+	 */
+	@Test
+	void testFormatDate() {
+		// null date
+		assertEquals(0, DateTimeUtils.formatDate(null, null, null, null).length());
+
+		// only date
+		Date sample = new Date();
+		assertEquals(new SimpleDateFormat().format(sample), DateTimeUtils.formatDate(sample, null, null, null));
+
+		// with format
+		String format = "MM/dd/yyyy HH:ss:SSS z";
+		assertEquals(new SimpleDateFormat(format).format(sample), DateTimeUtils.formatDate(sample, format, null, null));
+
+		// with locale
+		Locale locale = Locale.FRANCE;
+		assertEquals(new SimpleDateFormat(format, locale).format(sample),
+				DateTimeUtils.formatDate(sample, format, locale, null));
+
+		// with timezone
+		TimeZone timezone = TimeZone.getTimeZone("UTC");
+		SimpleDateFormat formatter = new SimpleDateFormat(format, locale);
+		formatter.setTimeZone(timezone);
+		assertEquals(formatter.format(sample), DateTimeUtils.formatDate(sample, format, locale, "UTC"));
+	}
+
 	/**
 	 * Test method for {@link DateTimeUtils#convertToTime(long)}.
 	 */
